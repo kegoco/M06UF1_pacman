39ac56c8415203bbf894223495ad2b28e665fcec
Kevin Gómez Codina <kevin.gomez.codina@gmail.com>
2018-10-31 18:17:41 +0100

    - Se ha implementado la opción para que los fantasmas puedan moverse por el tablero. - Se ha implementado la opción para derrotar al jugador.

--- pacman.js
+++ pacman.js
@@ -9,27 +9,15 @@ var objectsRef = {
 var player = {
     position: [0, 0]  // y - x
 };
-var ghosts = [{
-    position: [0, 0],  // y - x
-    last_position: [0, 0],
-    rutine: undefined
-}, {
-    position: [0, 0],  // y - x
-    last_position: [0, 0],
-    rutine: undefined
-}, {
-    position: [0, 0],  // y - x
-    last_position: [0, 0],
-    rutine: undefined
-}, {
-    position: [0, 0],  // y - x
-    last_position: [0, 0],
-    rutine: undefined
-}];
+var ghosts = [];
+var consoleInterval = undefined;
 
 /* Inicializa la aplicación */
 function init() {
     initBoard();
+    initPlayer();
+    initGhost();
+    visualizeOnConsole();
 }
 
 /* Inicializa el tablero */
@@ -65,8 +53,6 @@ function initBoard() {
     board[27]  = [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0];
     board[28]  = [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
     board[29]  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    
-    initPlayer();
 }
 
 /* Inicializa al jugador */
@@ -83,13 +69,11 @@ function initPlayer () {
             }
         }
     } while (!initialize);
-
-    initGhost();
 }
 
 /* Inicializa a los fantasmas */
 function initGhost () {
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < 3; i++) {
         var initialize = false;
         do {
             var x = Math.floor(Math.random() * (31 - 0)) + 0; 
@@ -97,16 +81,19 @@ function initGhost () {
             if (board[y] != undefined && board[y][x] != undefined) {
                 if (board[y][x] == 1) {
                     board[y][x] = 3;  // Especifica al fantasma en la casilla seleccionada.
-                    ghosts[i].position = [y, x];
-                    ghosts[i].last_position = ghosts[i].position;
-                    ghosts[i].rutine = initGhostRutine(i);
+                    ghosts.push({
+                        position: [y, x],  // y - x
+                        last_position: [y, x],
+                        rutine: initGhostRutine(i)
+                    });
+                    // ghosts[i].position = [y, x];
+                    // ghosts[i].last_position = ghosts[i].position;
+                    // ghosts[i].rutine = initGhostRutine(i);
                     initialize = true;
                 }
             }
         } while (!initialize);
     }
-
-    visualizeOnConsole();
 }
 
 /* Inicializa la rutina del fantasma */
@@ -116,16 +103,19 @@ function initGhostRutine(id_ghost) {
         var triesToMove = 0;
         do {
             var currentPosition = ghosts[id_ghost].position;
+            // Genera random entre [-1 y 1]
             var randX = Math.floor(Math.random() * (2 - (-1))) + (-1);
             var randY = Math.floor(Math.random() * (2 - (-1))) + (-1);
             
+            // Comprueba que la dirección sea horizontal o vertical
             if ((randX == 0 && randY == 1) || (randX == 1 && randY == 0) 
                 || (randX == -1 && randY == 0) || (randX == 0 && randY == -1)) {
+                // Le suma los valores aleatorios para crear la posición a la que se podrá mover el fantasma
                 var x = currentPosition[1] + randX;
                 var y = currentPosition[0] + randY;
 
                 if (board[y][x] == 1 && (y != ghosts[id_ghost].last_position[0] || x != ghosts[id_ghost].last_position[1])) {
-                    // El fantasma se mueve
+                    // El fantasma se mueve si hay camino y si es una dirección diferente de por donde ha venido
                     board[currentPosition[0]][currentPosition[1]] = 1;
                     board[y][x] = 3;
                     ghosts[id_ghost].last_position = ghosts[id_ghost].position;
@@ -134,10 +124,13 @@ function initGhostRutine(id_ghost) {
                 }
                 else if (board[y][x] == 2) {
                     // Jugador derrotado
-                    console.log("¡¡¡Jugador derrotado!!!");
+                    playerDefeated();
                     moved = true;
                 }
                 else if (board[y][x] == 3 && (y != ghosts[id_ghost].last_position[0] || x != ghosts[id_ghost].last_position[1])) {
+                    // Si el fantasma choca contra otro fantasma se le dará hasta un máximo de 12 oportunidades
+                    // para encontrar un nuevo camino. En caso de que no lo encuentre se le permitirá volver
+                    // sobre sus pasos.
                     triesToMove++;
                     if (triesToMove < 12) {
                         ghosts[id_ghost].last_position = [y, x];
@@ -145,12 +138,28 @@ function initGhostRutine(id_ghost) {
                 }
             }
         } while (!moved);
-    }, 2000, id_ghost);  // 2 segundos
+    }, 400, id_ghost);  // 2 segundos
+}
+
+/* Detiene el juego cuando el jugador es derrotado */
+function playerDefeated() {
+    console.log("¡¡¡Jugador derrotado!!!");
+    document.body.style.backgroundColor = "red";
+
+    // Para el intervalo de los enemigos
+    for (var i = 0; i < ghosts.length; i++) {
+        clearInterval(ghosts[i].rutine);
+        ghosts[i].rutine = undefined;
+    }
+
+    // Para el intervalo de actualizar el juego en la consola
+    clearInterval(consoleInterval);
+    consoleInterval = undefined;
 }
 
 /* Visualiza el tablero en la consola */
 function visualizeOnConsole() {
-    setInterval(() => {
+    consoleInterval = setInterval(() => {
         var container = document.getElementById("container");
         var result = "";
         for (var y = board.length - 1; y >= 0; y--) {
@@ -160,7 +169,7 @@ function visualizeOnConsole() {
             result += "\n";
         }
         console.log(result);
-    }, 2500);
+    }, 200);
 }
 
 init();
